fa2c91174d8d1083f3107123b5ff7dc818db98fc
leechanggi <lxyex1379@naver.com>
2020-12-20 18:05:13 +0900

    formal
    2020/12/20_18:05

--- js/main.js
+++ js/main.js
@@ -28,7 +28,70 @@ function allmenuFnc() {
     })
 }
 
+function swiperFnc() {
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 30,
+        slidesPerView: 3,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        effect: 'fade',
+        autoHeight: false,
+        fadeEffect: {
+            crossFade: true
+        },
+        loop: true,
+        loopedSlides: 5, //looped slides should be the same
+        speed: 1300,
+        autoplay: {
+            delay: 5000,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            renderFraction: function (currentClass, totalClass) {
+                return '<span class="' + currentClass + '"></span>' + '<span class="' + totalClass +
+                    '"></span>';
+            },
+        },
+        thumbs: {
+            swiper: galleryThumbs,
+        },
+        on: {
+            transitionEnd: function (swiper) {
+                $('.slide-bar').addClass('on');
+            },
+            slideChange: function () {
+                $('.slide-bar').removeClass('on');
+            }
+        }
+    });
+    var swiper = new Swiper('.newsroom-subslide', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    var swiper = new Swiper('.notice-subslide', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
+
 $(function () {
     gnbFnc();
     allmenuFnc();
+    swiperFnc();
 });
